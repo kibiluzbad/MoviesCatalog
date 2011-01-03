b79b2c7f705630acf2a2d5dfc577c13b1a10a527
Leonardo <kibiluzbad@hotmail.com>
2011-01-02 22:11:49 -0300

    Fixed start rating in ajax call.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,18 +1,22 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 $(document).ready(function(){
-   	$('.rating').raty({
-          readOnly: true,
-          path: "/images",
-          number: 10,
-          showHalf:true
-        });
-   	
-   	$('.rating').each(function(){
-   	    $(this).raty.start($(this).attr('data-rating'),"#" + $(this).attr('id'));
-   	});
+//   	$('.rating').raty({
+//          readOnly: true,
+//          path: "/images",
+//          number: 10,
+//          showHalf:true
+//        });
+//   	
+//   	loadRating();
    	
    	$(".pager .active a").click(function() { 
         return false;
    	});
 });
+
+function loadRating(){
+ $('.rating').each(function(){
+   	    $(this).raty.start($(this).attr('data-rating'),"#" + $(this).attr('id'));
+ });
+}
