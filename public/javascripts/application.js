fbd7bafe628d4f673305b327d6e59d0f8ec6638e
Leonardo <kibiluzbad@hotmail.com>
2011-01-02 22:15:41 -0300

    Fixed star rating bug on show.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,22 +1,7 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 $(document).ready(function(){
-//   	$('.rating').raty({
-//          readOnly: true,
-//          path: "/images",
-//          number: 10,
-//          showHalf:true
-//        });
-//   	
-//   	loadRating();
-   	
    	$(".pager .active a").click(function() { 
         return false;
    	});
 });
-
-function loadRating(){
- $('.rating').each(function(){
-   	    $(this).raty.start($(this).attr('data-rating'),"#" + $(this).attr('id'));
- });
-}
